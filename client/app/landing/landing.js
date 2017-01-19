9ebe4bc4b89b6e362464e720f3891fb24e606818
RJ Mohammad <rjmohammad@outlook.com>
2017-01-19 11:18:51 -0500

    login form

--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -1,3 +1,7 @@
 angular.module('app.landing', ['ngMaterial',"ng","ngAnimate","ngAria"])
-    .controller('LandCtrl', function ($scope, $location) {
+    .controller('LandCtrl', function ($scope, $location){
+        $scope.user = {}
+        $scope.login = function () {
+            console.log($scope.user)
+        }
     })
