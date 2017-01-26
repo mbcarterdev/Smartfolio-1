747124059ff36a39589369bd217b4f375af5a7a6
RJ Mohammad <RJ Mohammad>
2017-01-25 20:05:16 -0500

    got watson.js to save tags in database for faces

--- server/api/watson.js
+++ server/api/watson.js
@@ -1,34 +1,64 @@
-var visualV3 = require('watson-developer-cloud/visual-recognition/v3');
+var watson = require('watson-developer-cloud');
 var fs = require('fs');
 var db = require('../config/db');
+var path = require('path')
 
-// var visual_recognition = new VisualRecognotionV3 ({
-//     api_key: '<>',
-//     version_data: '2016-05-20'
-// });
+var visual_recognition = watson.visual_recognition({
+  api_key: '790f804248571cb7af13e9260d0d47df019b4498',
+  version: 'v3',
+  version_date: '2016-05-20'
+});
 
 
 db.raw(`SELECT images.idimages FROM images join tags on tags.idimages where images.idimages != tags.idimages`)
-.then( function (results) {
-    
+  .then(function (results) {
     results[0].forEach(function (imgid) {
-        console.log(imgid)
-        db.raw(`SELECT imghash from images where idimages  = ${imgid.idimages}`)
-        .then(function (result) {
-            results.forEach( function (imgUrl) {
-                imgUrl[0].imghash
-            })
+      db.raw(`SELECT imghash from images where idimages  = ${imgid.idimages}`)
+        .then(function (imgResult) {
+          var imgName = imgResult[0][0].imghash;
+          var params = {
+            images_file: fs.createReadStream(`../uploads/${imgName}`)
+          };
+
+          visual_recognition.detectFaces(params, function (err, res) {
+            if (err) {
+              console.log(err);
+            } else {
+              res.images[0].faces.forEach( function(face) {
+                var age;
+                if(face.age.max) {
+                 age = `age\: ${face.age.min} - ${face.age.max}`;
+                } else {
+                  age = `age\: around ${face.age.min}`;
+                }
+                db.raw(`INSERT INTO tags values (null, ${imgid.idimages} ,'${age}')`)
+                .then(function () {
+                  db.raw(`INSERT INTO tags values (null, ${imgid.idimages} ,'${face.gender.gender}')`)
+                })
+              })
+
+              // visual_recognition.classify(params, function (err, res) {
+              //   if (err)
+              //     console.log(err);
+              //   else
+              //     console.log(JSON.stringify(res, null, 2));
+              // });
+            }
+              
+         });
         });
     });
-})
-// var params = {
+  })
 
+// var params = {
+//   images_file: fs.createReadStream('../uploads/michelle@michelle.com download-free-coreldraw-tutorials-vector-design.png')
 // };
 
-// visua.classify(params, function (err, res) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log(JSON.stringify(res,null, 2))
-//     }
-// });
+// visual_recognition.detectFaces(params,
+//   function(err, response) {
+//     if (err)
+//       console.log(err);
+//     else
+//       console.log(typeof response);
+//       console.log(JSON.stringify(response,null,2));
+//   });
