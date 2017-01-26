8fe63386a113bbb0e00266dec7be75514c872469
RJ Mohammad <RJ Mohammad>
2017-01-25 23:41:30 -0500

    can add tags now to the database

--- server/api/watson.js
+++ server/api/watson.js
@@ -10,7 +10,7 @@ var visual_recognition = watson.visual_recognition({
 });
 
 
-db.raw(`SELECT images.idimages FROM images join tags on tags.idimages where images.idimages != tags.idimages`)
+db.raw(`SELECT images.idimages from images where images.idimages not in  (select tags.idimages from tags)`)
   .then(function (results) {
     results[0].forEach(function (imgid) {
       db.raw(`SELECT imghash from images where idimages  = ${imgid.idimages}`)
@@ -24,30 +24,62 @@ db.raw(`SELECT images.idimages FROM images join tags on tags.idimages where imag
             if (err) {
               console.log(err);
             } else {
-              res.images[0].faces.forEach( function(face) {
+              res.images[0].faces.forEach(function (face) {
                 var age;
-                if(face.age.max) {
-                 age = `age\: ${face.age.min} - ${face.age.max}`;
+                if (face.age.max) {
+                  age = `age\: ${face.age.min} - ${face.age.max}`;
                 } else {
                   age = `age\: around ${face.age.min}`;
                 }
-                db.raw(`INSERT INTO tags values (null, ${imgid.idimages} ,'${age}')`)
-                .then(function () {
-                  db.raw(`INSERT INTO tags values (null, ${imgid.idimages} ,'${face.gender.gender}')`)
+                db.raw(`INSERT INTO tags VALUES (null, ${imgid.idimages} ,'${age}')`)
+                .then( function (results) {
+                  console.log('more success')
                 })
+                .catch( function (err) {
+                  console.log(err)
+                })
+                db.raw(`INSERT INTO tags VALUES (null, ${imgid.idimages} ,'${face.gender.gender}')`)
+                .then( function (results) {
+                  console.log('success')
+                })
+                .catch( function (error) {
+                  console.log('error')
+                })
+                
               })
-
-              // visual_recognition.classify(params, function (err, res) {
-              //   if (err)
-              //     console.log(err);
-              //   else
-              //     console.log(JSON.stringify(res, null, 2));
-              // });
             }
-              
-         });
-        });
+
+          });
+
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+      db.raw(`SELECT imghash from images where idimages  = ${imgid.idimages}`)
+        .then(function (imgResult) {
+          var imgName = imgResult[0][0].imghash;
+          var params = {
+            images_file: fs.createReadStream(`../uploads/${imgName}`)
+          };
+          visual_recognition.classify(params, function (err, res) {
+            if (err) {
+              console.log(err);
+            } else {
+              res.images[0].classifiers[0].classes.forEach(function (tagClass) {
+
+                db.raw(`INSERT INTO tags VALUES (null, ${imgid.idimages}, '${tagClass.class}')`)
+                  .then(function (results) {
+                  })
+                  .catch(function (err) {
+                    console.log(err)
+                  })
+              });
+            }
+          });
+        })
     });
+  }).catch(function (err) {
+    console.log(err)
   })
 
 // var params = {
