9f7301a9d82a9067f8a0e34de80e1a34b751a95f
RJ Mohammad <rjmohammad@outlook.com>
2017-01-29 23:28:49 -0500

    finished layout for settings page

--- client/app/settings/settings.js
+++ client/app/settings/settings.js
@@ -1,4 +1,14 @@
 angular.module('app.settings', ['ngMaterial', "ng", "ngAnimate", "ngAria"])
-  .controller('SettingsCtrl', function ($scope) {
-
+  .controller('SettingsCtrl', function ($scope, $mdSidenav, Auth, $location) {
+    $scope.pageClass = 'page settings';
+    $scope.home = function () {
+      $location.path('/home');
+    }
+    $scope.toggleLeft = buildToggler('left');
+     function buildToggler(componentId) {
+      return function () {
+        $mdSidenav(componentId).toggle();
+      }
+    };
+    $scope.logoff = Auth.signout;
   });
