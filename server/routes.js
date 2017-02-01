ee6e24364e40e0789658447895be4d6aaf61d030
xutopia <xuology@gmail.com>
2017-02-01 11:17:21 -0500

    added routes for albums, gitignored DS_Store files

--- server/routes.js
+++ server/routes.js
@@ -1,14 +1,21 @@
 var uController = require('./controllers/userController.js');
 var iController = require('./controllers/imgController.js');
+var aController = require('./controllers/albumController.js');
 var helper = require('./authenticator/authenticator');
 
 module.exports = function (app, express) {
   app.post('/signin', uController.signin);
   app.post('/register/', uController.register);
-  app.get('/photos',helper.decode, iController.fetch);
-  app.post('/upload/photos',helper.decode, iController.upload);
+  app.get('/photos', helper.decode, iController.fetch);
+  app.post('/upload/photos', helper.decode, iController.upload);
   app.get('/photos/:imgurl', iController.serve);
   app.delete('/photos/:imgurl', helper.decode, iController.delete);
+  app.get('/albums' /* functions */);
+  app.get('/albums/:albumurl' /* functions */);
+  app.post('/upload/albums' /* functions */);
+  app.put('/albums' /* functions */);
+  app.delete('/albums/:albumurl' /* functions */);
+  app.put('/albums/:imgurl' /* functions */);
   //app.put for changing the front or back img
     //will this go to /photos or /upload/photos?
     //will need a method on iController for updating images
