674688c221e0c243f8447e8631034792c7f35e3b
xutopia <xuology@gmail.com>
2017-02-01 14:56:49 -0500

    complete fetch method with nested queries. No idea if it works. Will need to test with Postman. Onwards to front-end before finishing album methods

--- server/controllers/albumController.js
+++ server/controllers/albumController.js
@@ -5,6 +5,28 @@ var fs = require('fs');
 module.exports = {
   fetch: function(req, res) {
     // fetch all albums belonging to the user
+    var username = req.headers.username;
+    var data = [];
+    db.raw(`SELECT idusers FROM smartfolio.users WHERE username = '${username}'`)
+    .then(function(userinfo) {
+      var userID = userInfo[0][0].idusers;
+      db.raw(`SELECT * FROM smartfolio.albums WHERE userID = '${userID}'`)
+      .then(function(albuminfo) {
+        albuminfo[0].forEach(function(album, index) {
+          db.raw(`SELECT idimages FROM smartfolio.images INNER JOIN album-image ON album-image.imageID=images.idimages WHERE album-image.albumID=${album.idalbums}`) // need to write a comment here to explain the query string
+          .then(function(images) {
+            album['images'] =       images[0].map(function(imageObj) {
+              return tagObj.tag;
+            });
+            data.push(album);
+
+            if(index === albuminfo[0].length - 1) {
+              res.status(200).send(data);
+            }
+          })
+        })
+      })
+    })
   },
 
   serve: function(req, res) {
