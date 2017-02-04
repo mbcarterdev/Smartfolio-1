bb47561e284223969104781ba112e9e60ad736f1
Rishi Shah <shah.rishi05@gmail.com>
2017-02-04 16:08:15 -0500

    Updates to ShareModalCtrl

--- client/app/app.js
+++ client/app/app.js
@@ -77,7 +77,11 @@ angular.module('app', ['app.landing',
   .controller('CreateAlbumCtrl', function($scope, close, Collage, Albums) {
 
   })
-  .controller('ShareModalCtrl', function ($scope, close, Pics, Collage) { // takes the files from the dialogbox and sends it to server
+  .controller('ShareModalCtrl', function ($scope, close, Shared, Pics, Collage) { // takes the files from the dialogbox and sends it to server
+
+    $scope.input = '';
+
+    $scope.usersToAdd = [];
 
     var fd = new FormData();
     $scope.uploadFile = function (fileType, files) {
